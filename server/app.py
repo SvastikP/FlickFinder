a5b91078d7039054b775d450fec29c99f74a67b9
Ahkhan1815 <ahkhan1815@gmail.com>
2025-11-27 19:34:34 -0600

    added db script

--- server/app.py
+++ server/app.py
@@ -1,6 +1,29 @@
 from flask import Flask
+import pandas as pd
+import sqlite3
+from pathlib import Path
 
+
+def createDB():
+    DATA_DIR = Path(__file__).parent
+    RATINGS_SMALL_CSV = DATA_DIR / "ratings.csv"
+    MOVIES_CSV = DATA_DIR / "movies_metadata.csv"
+    DB_PATH = DATA_DIR / "movies.db"
+    movies = pd.read_csv(MOVIES_CSV)
+    ratings = pd.read_csv(RATINGS_SMALL_CSV)
+
+    conn = sqlite3.connect(DB_PATH)
+
+    movies.to_sql("movies_metadata", conn, if_exists="replace", index=False)
+    ratings.to_sql("ratings", conn, if_exists="replace", index=False)
+
+    conn.close()
+    print("Done")
+
+createDB()
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def test():
